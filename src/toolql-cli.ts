d61a12c9a5842052f39ee6164b688f5a1941d414
tstewart <tstewart@klaudhaus.com>
2025-05-21 15:51:53 +1000

    Interactive CLI.

--- src/toolql-cli.ts
+++ src/toolql-cli.ts
@@ -1,7 +1,30 @@
 import "dotenv/config"
+import readline from "node:readline"
+import { createInterface } from 'node:readline';
+import { exit, stdin, stdout } from 'node:process';
 
 export const main = () => {
   const openAiApiKey = process.env.TOOLQL_OPENAI_API_KEY
   if (!openAiApiKey) throw new Error("OpenAI API key missing, please set TOOLQL_OPENAI_API_KEY")
-  console.log("Hello from ToolQL")
+
+	const rl = createInterface({
+		input: stdin,
+		output: stdout,
+		prompt: '- ',
+	});
+
+	console.log("Hi there, how can I help?")
+	rl.prompt();
+
+	rl.on('line', (line) => {
+		console.log("I'll get back to you on that one! Anything else?")
+		rl.prompt();
+	}).on('close', () => {
+		console.log("")
+		console.log('Thanks, see you soon!');
+		console.log('For all your GraphQL / A.I. integration needs visit https://toolql.com');
+		exit(0);
+	});
 }
+
+
