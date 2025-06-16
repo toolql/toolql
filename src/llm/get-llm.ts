d4ff4140af3f4da34a58efe5d0767530ef186c55
tstewart <tstewart@klaudhaus.com>
2025-06-16 18:31:50 +1000

    Clarify getLlm scope.

--- src/llm/get-llm.ts
+++ src/llm/get-llm.ts
@@ -9,7 +9,13 @@ export type LlmParams = BaseChatModelParams
 export const MISSING_API_KEY_ERROR =
   "OpenAI API key missing, please set OPENAI_API_KEY"
 
-// TODO: Extends to support other parameterised models
+/**
+ * Obtain an LLM for sample agents.
+ * When using the LangChain API this can be replaced with any LangChain supported LLM.
+ * When using MCP, LLM is specified by the MCP consuming agent.
+ *
+ * TODO: Extend to support other providers and models.
+ */
 export const getLlm = (): BaseChatModel => {
   const openAiApiKey = process.env.OPENAI_API_KEY
   if (!openAiApiKey) throw new Error(MISSING_API_KEY_ERROR)
